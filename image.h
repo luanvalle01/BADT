31671207a54deef5a723b2ae13c2d04041091e95
Luan_Valle <82781125+luanvalle01@users.noreply.github.com>
2023-11-09 01:55:15 -0300

    Frames Primitivos 100% funcionais
    --09 de Novembro de 2023--
    🧭️Visão geral:
    	✅️Estrutura de imagens completamente funcinal.
    	✅️Estrutura de frames primitivos completamente funcional.
    	✅️Frames "menu" completamente funcionais.
    
    	🗓️Próximas atividades:
    		•Criar estrutura de tabelas.
    		•Criar modo "table" para frames.
    		 >Implementar métodos construtores.
    		•Criar modo "input" para frames.
    		 >será armazenado em frame.inputBuffer e terá um limite de caracteres definido pelo desenvolvedor.
    		•Criar modo "form" para frames.
    		 >Estrutura semelhante a menu, onde cada linha representa uma característica a ser cadastrada.
    		 >Relação direta com a tabela atribuída.
    
    🗺️Alterações no escopo:
    	•⚠️Recomendação: para alterar estado do programa, getch() pode ser comparado com 10(decimal de enter).
    	•⚠️Internamente, os modos são reconhecidos apenas pela primeira letra de seu nome.
    	•Program terá variável breaK, para indicar finalização do programa.
    	•Existirão protótipos de métodos para serem definidos pelo desenvolvedor:
    		>programUpdadte - chamado uma vez a cada tick.
    		>programFUpdate - chamado a cada x intervalos de tempo.
    	•Adicionado msCounter e msUpdate em program para funcionar em conjunto com programFUpdate.
    	•msUpdate inicia em 16ms.
    	•⚠️getch (multOS) adicionado ao projeto (fonte devidamente creditada)
    
    🚧️Alterações em str.h:
    	•Definido constante true = 1.
    	•Definido constante false = 0.
    	•Criado strCat. (necessita de realloc externo)
    
    🚧️Adições em frames.h:
    	•Adicionado char *options na estrutura de frames. 100% funcioal
    		>"opcao1|opcao2|".
    	•Adicionado int qtdOptions na estrutura de frames.
    
    	•frameSetCursor agora é 100% funcional.
    
    	•Implementado frameInsert - 100% funcional.
    	•frameInsertCenter - 100% funcional.
    	•Criado frameInsertOption - escreve próxima opção.
    	•Criado frameCleanOption - remove todas opções.
    	•frameCursorAdd adicionado.
    	•frameCursorSub adicionado.
    	•⚠️Dinâmica de opção selecionada funcional.
    		>Todo frame do modo "menu" armazena a posição atualmente selecionada.
    
    	•frameRefresh obsoleto. (preservado para possível uso)
    	•frameFree 100%
    
    	•Bug no frame insert após inserir opções solucionado.
    	•Bug no frame insert center que sobreescrevia última atualização solucionado.

---
+++ image.h
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "str.h"
+
+typedef struct{
+	char *pixels;
+	int width, height;
+}Image;
+
+void imageSetup(Image *image, int width, int height, char _realoc);
+
+void imageShow(Image *image);
+
+void imageClean(Image *image);
+void imageCopy(Image *imageDestiny,Image  *imageOrigin, int x, int y);
+void imageResize(Image *image, int newWidth, int newHeight);
+
+void imageInsert(Image *image, char *string, int x, int y);
+
+void freeImage(Image *image);
+
+//-------//
+
+void imageSetup(Image *image, int width, int height, char _realloc){
+
+	int i = 0;
+	int size = width * height + 1;
+
+	if(_realloc == 'y')
+		image->pixels = (char *)realloc(image->pixels, size);
+	else
+		image->pixels = (char *)malloc(size);
+
+	for(int i = 0; i < size; i++)
+		image->pixels[i] = ' ';
+
+	image->pixels[i] = '\0';
+	image->width = width;
+	image->height = height;
+}
+
+void imageShow(Image *image){
+
+	int size = image->width * image->height;
+	int endLine = image->width -1;
+
+	for(int i = 0; i < size; i++){
+		if(i % endLine == 0)
+			printf("\n");
+
+		printf("%c", image->pixels[i]);
+	}
+}
+
+void imageClean(Image *image){
+
+	int i = 0;
+	int size = image->width * image->height + 1;
+
+	for(int i = 0; i < size; i++)
+		image->pixels[i] = ' ';
+
+	image->pixels[i] = '\0';
+}
+
+void imageCopy(Image *imageDestiny, Image  *imageOrigin, int x, int y){
+
+	int i = 0, cursorDestiny = 0, cursorOrigin = 0;
+	int size = imageOrigin->width * imageOrigin->height;
+	int diferenceW;
+	char _realloc = 'n';
+
+	//RESIZE
+	if(imageOrigin->width + x > imageDestiny->width)
+		_realloc = 'y';
+
+	if(imageOrigin->height + y > imageDestiny->height)
+		_realloc = 'y';
+
+	if(_realloc == 'y')
+		imageSetup(imageDestiny,  imageOrigin->width + x, imageOrigin->height + y, _realloc);
+
+	//CURSOR SEEK
+	cursorDestiny = y * imageDestiny->width + x;
+	diferenceW = imageDestiny->width - (imageOrigin->width + x);
+
+
+	//COPY
+	for(i = 0; i < size; i++){
+		if(cursorOrigin % (imageOrigin->width - 1) == 0 && cursorOrigin != 0)
+			for(int j = 0; j < diferenceW; j++)
+				cursorDestiny++;
+		imageDestiny->pixels[cursorDestiny++] = imageOrigin->pixels[cursorOrigin++];
+	}
+}
+
+void imageResize(Image *image, int newWidth, int newHeight){
+
+	int newSize = (newWidth * newHeight) +1;
+
+	Image imageBuffer;
+	imageSetup(&imageBuffer, image->width, image->height, 'n');
+	imageCopy(&imageBuffer, image, 0, 0);
+
+	image->pixels = realloc(image->pixels, newSize);
+	imageSetup(image, newWidth, newHeight, 'y');
+
+	imageCopy(image, &imageBuffer, 0, 0);
+}
+
+
+void imageInsert(Image *image, char *string, int x, int y){
+
+	int cursor = (image->width * y) + x;
+	int size = strLen(string);
+
+	if(image->width < size + x)
+		imageResize(image, size + x + 1, image->height);
+
+	for(int i = 0; i < size; i++)
+		image->pixels[cursor++] = string[i];
+}
+
+void imageFree(Image *image){
+	free(image->pixels);
+}
