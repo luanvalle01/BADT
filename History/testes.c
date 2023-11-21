7cd5f2f317d90dc2374548298f982412b594a3ea
Luan_Valle <82781125+luanvalle01@users.noreply.github.com>
2023-11-21 02:35:16 -0300

    1.0 🎉
    --20 de Novembro de 2023--
    
    🧭️Visão geral:
    	✅️Estrutura de imagens completamente funcional.
    	✅️Estrutura de frames primitivos completamente funcional.
    	✅️Frames "menu" completamente funcionais.
    	✅️Estrutura de tabelas funcional, atuando diretamente na gestão de arquivos.
    	✅️Modo "table" para frames completamente funcional.
    	✅️Modo "input" para frames completamente funcional.
    
    	🗓️Próximas atividades:
    		•Adicionar char *Footer para atuar como "legenda" dos frames.
    		•Criar modo "form" para frames.
    			>Estrutura semelhante a menu, onde cada linha representa uma característica a ser cadastrada.
    			>Relação direta com a tabela atribuída.
    		•Criar estrutura program.
    
    🚧️Alterações em str.h:
    	•Criada função para converter qualquer valor para texto.
    	•Implementado strLenPlus, que permite medir textos com terminaodores específicos.
    	•Criado bufferClean para preencher char * com '\0';
    
    🚧️Alterações em frame.h:
    	•frameSetSubTitle criado e vinculado à frameRefresh de todos os tipos possíveis;
    	•frameSetTable implementado.
    
    	•Criado frameTableTitles para formatar títulos da coluna das tabelas atribuídas.
    	•frameRefreshMenu 100% funcional.
    	•frameRefreshTable 100% funcional.
    	•frameRefresh será usado para gerenciar modos (função separada em capsulas).
    	•Criado frameSetInput para inicializar frame no modo input.
    	•Criado frameInputAdd para adicionar um char ao buffer.
    	•Criado frameInputSub para remover último char do buffer.
    	⚠️Basta atribuir input em frame.inputBuffer[0] para atulizar visualização da tabela automaticamente.
    
    🚧️Alterações em table.h:
    	•Estrutura de tabela criada.
    	•Criado tableSetup.
    	•Criado tableRealloc.
    	•Criado tableCopy para auxiliar na atualizalçao dos frames.
    
    	•Criado tableNewLine.
    	•Criado tableCursor para calcular index de determinado registro.
    	•Criado tableBuffer para copiar a tabela a partir de determinado registro (isso também limpa a tabela a partir do cursor).
    
    	•Criada função tableIsert para identificar automaticamente o tipo da coluna a inserir valor
    	•Função tableIsertT 100% funcional (int, float, char).
    	•Função tableInsertS criada para inserir strings 100% funcional (se atentar a alocação adequada da string que será usada).
    
    	•Função tableDelete criada para funcionar em conjunto com tableUpdate 100% funcional.
    	•Criada tableUpdate pra atualizar cadastros 100% funcional.
    
    	•Criada tableGetJump para retornar o cursor do início da próxioma página da tabela.
    
    	•Criada tableToTxt 100% funcional.
    	•Criada txtToTable 100% funcional.
    
    	•Criada tableRefresh para atuar com frameRefres.
    
    	•Criada tableGet para obter valor de determinada célula (saída em char *, necessita conversão com funções at).
    
    	•Criado tableFree para liberar todos os alocamentos dinâmicos.
    
    	⚠️tableInsertS possuí bug que limita a quantidade de caracteres a 74.

---
+++ History/testes.c
@@ -0,0 +1,104 @@
+#include "frame.h"
+
+void insertOption(Frame *frame){
+	char optionS[30];
+	sprintf(optionS, "Opcao selecionada: %i", frame->optionSelect);
+        optionS[29] = '\0';
+        frameInsertCenter(frame,optionS, 3);	
+}
+
+void mainMenu(Frame *frame){
+
+	frameSetup(frame,"menu", 50, 10,'n');
+	frameInsert(frame, "escrevendo normal...", 1,2);
+	frameInsertCenter(frame,"(centralizado)", 3);
+	
+	frameInsertOption(frame,"opcao 1");
+	frameCleanOption(frame);
+	frameInsertOption(frame,"ver tabela");
+	frameInsertOption(frame,"ver input");
+	frameInsertOption(frame,"Sair");
+	
+	frameInsert(&frame[0], "W - cima", 1,9);
+	frameInsert(&frame[0], "D - baixo", 1,10);
+}
+
+void main(){
+	int state = 0, run = 1;
+	
+	Frame frame[3];
+	
+	mainMenu(&frame[0]);
+	Table table;
+	tableSetup(&table, "dscif","id,nome,letra,numero,dinheiro");
+	txtToTable(&table,"./teste");
+	
+	frameSetup(&frame[1],"tabela", 5, 20,'n');
+	frameSetTable(&frame[1], &table);
+	frameRefresh(&frame[1]);
+	
+	frameSetup(&frame[2],"input", 10, 5,'n');
+	frameSetInput(&frame[2], "Digite seu CPF (apenas numeros)", 10);
+	
+	char buffer = '\0';
+	float testeSOMA = 10;
+	
+	while(run){		
+	
+		frame[state].inputBuffer[0] = buffer;
+		frameRefresh(&frame[state]);
+		insertOption(&frame[0]);
+		
+		clear();
+		imageShow(&frame[state].image);
+		
+      	if(buffer != 10)
+			buffer = getch();
+     	
+     	if(!state){
+     		
+	        if(buffer == 10){
+				if(frame[0].optionSelect == 0)
+					state = 1;
+				if(frame[0].optionSelect == 1)
+					state = 2;
+				if(frame[0].optionSelect == 2)
+					run = false;
+
+				buffer = '\0';
+			}
+
+		}
+		else if(state == 1){
+			if(buffer == 'd' || buffer == 'D'){
+				testeSOMA += .10;
+				tableUpdate(&table, 4, frame[1].table->actualPage, &testeSOMA);
+			}
+
+			if(buffer == 10){
+				state = 0;
+				buffer = '\0';
+			}
+		}
+		else if(state == 2){
+			if(buffer == 10){
+				state = 0;
+				buffer = '\0';
+			}
+		}
+	}
+
+	clear();
+	
+	frameFree(&frame[0]);
+	frameFree(&frame[1]);
+	tableFree(&table);
+	frameFree(&frame[2]);
+	
+	printf("====================================\n");
+	printf("|                                  |\n");
+	printf("| Programa encerrado corretamente! |\n");
+	printf("|                                  |\n");
+	printf("====================================\n");
+
+}
